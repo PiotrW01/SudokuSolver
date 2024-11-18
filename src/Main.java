b6d8555fb1464013a358a7297330694a9a053cdd
PiotrW01 <piotr140401@gmail.com>
2024-11-18 21:11:21 +0100

    Add cli arguments

--- src/Main.java
+++ src/Main.java
@@ -5,13 +5,25 @@ import java.awt.*;
 
 public class Main {
     public static void main(String[] args) {
+
         JFrame frame = new JFrame();
         SudokuPanel panel = new SudokuPanel();
         frame.add(panel.GetPanel(), BorderLayout.CENTER);
         SetupWindow(frame);
-
         SudokuSolver solver = new SudokuSolver(panel.GetCells());
-        solver.Start(SudokuSolver.Type.BRUTE_FORCE);
+
+        SudokuSolver.Type type = SudokuSolver.Type.BRUTE_FORCE;
+        if(args.length > 0){
+            switch(args[0]){
+                case "-b":
+                    type = SudokuSolver.Type.BRUTE_FORCE;
+                    break;
+                case "-c":
+                    type = SudokuSolver.Type.CONSTRAINT_PROPAGATION;
+                    break;
+            }
+        }
+        solver.Start(type);
     }
 
     private static void SetupWindow(JFrame frame){
